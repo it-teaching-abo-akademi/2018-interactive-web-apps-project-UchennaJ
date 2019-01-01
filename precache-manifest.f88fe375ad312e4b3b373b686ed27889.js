9a7191b56ee182a034ae71b5f77604ce9a1da8f4
UchennaJ <33607024+UchennaJ@users.noreply.github.com>
2019-01-02 00:03:07 +0200

    Updates

---
+++ precache-manifest.f88fe375ad312e4b3b373b686ed27889.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d08680b28d1889e26fc7",
+    "url": "/2018-interactive-web-apps-project-UchennaJ/static/js/runtime~main.d08680b2.js"
+  },
+  {
+    "revision": "b7983ab281c728ddb4b1",
+    "url": "/2018-interactive-web-apps-project-UchennaJ/static/js/main.b7983ab2.chunk.js"
+  },
+  {
+    "revision": "108fbe4e5b475c38061d",
+    "url": "/2018-interactive-web-apps-project-UchennaJ/static/js/1.108fbe4e.chunk.js"
+  },
+  {
+    "revision": "b7983ab281c728ddb4b1",
+    "url": "/2018-interactive-web-apps-project-UchennaJ/static/css/main.389d6b72.chunk.css"
+  },
+  {
+    "revision": "b995e31de9deb3f42514d268da2e3472",
+    "url": "/2018-interactive-web-apps-project-UchennaJ/index.html"
+  }
+];
